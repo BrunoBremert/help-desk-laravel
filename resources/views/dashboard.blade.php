fcb61c97a2dcc52107c22f02d7649e6135067a20
Bruno Bremert <brunog.bremert@gmail.com>
2026-03-28 20:32:35 +0000

    commit inicial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Título</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Status</th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Prioridade</th>
-                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data</th>
+                            <th class="px-6 py-3 text-left text                            git push -u origin master-xs font-medium text-gray-500 uppercase">Data</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200">
